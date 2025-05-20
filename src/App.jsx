c4146cbe9f0149bace511503e0242ae40aea74f4
angela142004 <elvaromanimolina@gmail.com>
2025-05-20 17:18:42 -0500

    create: Hize el navbar_Angela

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Button from './componets/button';
-
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./componets/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-green-700 font-bold" >PRISMA</h1>
-      <div className="card">
-        <p>
-          Este es el inicio de la pagina prisma por chicos de <code>undc</code>
-        </p>
-      </div>
-
-      <p className='font-bold'>Esto es una prueba</p>
-      <Button onClick={()=>console.log("Hola")} >touch me</Button>
-      
+      <Navbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
