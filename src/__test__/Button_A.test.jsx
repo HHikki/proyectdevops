603c69fabad693fc491c4b5b3f2d00c2d010a701
Rabanito-P <2201080337@undc.edu.pe>
2025-05-20 20:19:39 -0500

    nuevos text

--- src/__test__/Button_A.test.jsx
+++ src/__test__/Button_A.test.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "../App";
+import Button_A from "../components/Button_A";
 
 test("Renderiza sin errores", () => {
-  render(<App />);
+  render(<Button_AButton_A/>);
   //expect(screen.getByRole("heading")).toBeInTheDocument();
 });
