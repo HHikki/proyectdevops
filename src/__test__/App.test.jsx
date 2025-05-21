10437a947335d201d1e6446576d6a73b6e2dbc5b
angela142004 <elvaromanimolina@gmail.com>
2025-05-21 17:12:42 -0500

    add: browser_angela

--- src/__test__/App.test.jsx
+++ src/__test__/App.test.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../App";
+import { BrowserRouter } from "react-router-dom";
 
 test("Renderiza sin errores", () => {
-  render(<App />);
+  <BrowserRouter>
+    render(
+    <App />
+    );
+  </BrowserRouter>;
+
   //expect(screen.getByRole("heading")).toBeInTheDocument();
 });
