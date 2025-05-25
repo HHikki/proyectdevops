3ae28c1702563b7ce51248650433cac33829ec92
angela142004 <elvaromanimolina@gmail.com>
2025-05-24 21:51:46 -0500

    Añadir el modulo de swiper

--- jest.config.cjs
+++ jest.config.cjs
@@ -6,8 +6,12 @@ module.exports = {
     "\\.(svg|png|jpg|gif)$": "<rootDir>/__mocks__/fileMock.js",
     "\\.(css|scss)$": "identity-obj-proxy",
   },
+  transformIgnorePatterns: [
+    "node_modules/(?!swiper)", // <--- permite que Jest transforme swiper
+  ],
   transform: {
-    "^.+\\.jsx?$": "babel-jest",
+    "^.+\\.[jt]sx?$": "babel-jest", // js, jsx, ts, tsx
+    "^.+\\.mjs$": "babel-jest", // <--- añade esta línea
   },
   testMatch: [
     "**/__tests__/**/*.test.[jt]s?(x)",
